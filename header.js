19781ffea5e57d7df30720459a7e4456029f056f
cprado2021 <88091543+cprado2021@users.noreply.github.com>
2021-07-28 08:24:19 -0500

    Update header.js

--- header.js
+++ header.js
@@ -1,5 +1,31 @@
-$(function () {
-  $('#myiframe').load(function () {
-    $(this).contents().find('#suiteBarTop').hide();
-  });
-})...
+window.addEventListener('load', function () {
+	function go() {
+		i = i < width ? i + step : 1;
+		m.style.marginLeft = -i + 'px';
+	}
+	var i = 0,
+		step = 3,
+		space = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
+	var m = document.getElementById('marquee');
+	var t = m.innerHTML; //text
+	m.innerHTML = t + space;
+	m.style.position = 'absolute'; // http://stackoverflow.com/questions/2057682/determine-pixel-length-of-string-in-javascript-jquery/2057789#2057789
+	var width = (m.clientWidth + 1);
+	m.style.position = '';
+	m.innerHTML = t + space + t + space + t + space + t + space + t + space + t + space + t + space;
+	m.addEventListener('mouseenter', function () {
+		step = 0;
+	}, true);
+	m.addEventListener('mouseleave', function () {
+		step = 3;
+	}, true);
+	var x = setInterval(go, 50);
+}, true);
+#marquee {
+   background:#eee;
+   overflow:hidden;
+   white-space: nowrap;
+ }
+<div id="marquee">
+	1 Hello world! 2 Hello world! <a href="#">3 Hello world!</a>
+</div>
